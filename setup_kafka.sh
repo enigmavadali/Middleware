96a872d3f84b61be4902e8b57e5bcec377b6b65d
Rohit Vadali <vadalirohit@gmail.com>
2024-01-27 23:38:05 -0800

    Add topics to kafka setup

--- setup_kafka.sh
+++ setup_kafka.sh
@@ -16,8 +16,19 @@
 
 echo "Kafka broker is now healthy. Running setup"
 
-/opt/kafka_2.13-2.8.1/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic $TEST_TOPIC_NAME
-echo "topic $TEST_TOPIC_NAME was created"
+echo "Deleting Topics"
+
+/opt/kafka_2.13-2.8.1/bin/kafka-topics.sh --zookeeper zookeeper:2181 --delete --topic $RESIDENT_TOPIC
+
+/opt/kafka_2.13-2.8.1/bin/kafka-topics.sh --zookeeper zookeeper:2181 --delete --topic $SENSOR_TOPIC
+
+echo "Creating Topics"
+
+/opt/kafka_2.13-2.8.1/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic $RESIDENT_TOPIC
+echo "topic $RESIDENT_TOPIC was created"
+
+/opt/kafka_2.13-2.8.1/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic $SENSOR_TOPIC
+echo "topic $SENSOR_TOPIC was created"
 
 # for message in "cat sample_messages.txt"; do
 #     echo "$message" | /opt/kafka_2.13-2.8.1/bin/kafka-console-producer.sh --topic $TEST_TOPIC_NAME --bootstrap-server localhost:9092
